756739fd19a5be5072e953b399305da83a52cff1
IvynaSantino <ivyna.alves@ccc.ufcg.edu.br>
2017-11-12 23:10:26 -0200

    add de criptogragia e descriptografia nas charadas

--- engimacesar.cpp
+++ engimacesar.cpp
@@ -16,21 +16,24 @@ using namespace std;
 */
 
 
+// Falta ver se aceita chave 0 de deslocamento!
+// Falta banco de perguntas-charadas medio e dificil!
+
 // Banco de perguntas-charadas
-string perguntasFacil[10] = {"Mantem sempre o mesmo tamanho, nao importa o peso?",
-"O que detestamos na praia e adoramos na panela?",
-"O que eh que anda com os pe na cabeça?",
-"O que eh, o que e? Que é irma de minha tia e nao eh minha tia?",
-"O que eh, o que eh? De dia tem quatro pes e de noite tem seis?",
-"O que eh, o que eh? Anda deitado e dorme em pe?",
-"Qual o sobrenome que todo mundo tem?",
-"Quem morre em pe?",
-"Tem cabeça e nao eh gente, tem dente e nao eh pente?",
-"O que eh que nasce grande e morre pequeno?"};
+string perguntasFacil[10] = {"mantem sempre o mesmo tamanho, nao importa o peso?",
+"o que detestamos na praia e adoramos na panela?",
+"o que eh que anda com os pe na cabeca?",
+"o que eh, o que eh? que eh irma de minha tia e nao eh minha tia?",
+"o que eh, o que eh? de dia tem quatro pes e de noite tem seis?",
+"o que eh, o que eh? anda deitado e dorme em pe?",
+"qual o sobrenome que todo mundo tem?",
+"quem morre em pe?",
+"tem cabeca e nao eh gente, tem dente e nao eh pente?",
+"o que eh que nasce grande e morre pequeno?"};
 
 // Respostas das perguntas-charadas
 string respostasFacil[10] = {"balanca", "caldo", "piolho", "mae", "cama", "pe", "costa", "vela", "alho", "lapis"};
-
+string alfa = "abcdefghijklmnopqrstuvwxyz";
 
 void telaInicial();
 void regras();
@@ -39,6 +42,8 @@ int indiceRandomico();
 int limiteIndice();
 string escolheFraseFacil();
 void imprimeCharadaFacil();
+string crip(string frase, int chave);
+string descrip(string frase, int chave);
 
 int main() {
     telaInicial();
@@ -96,11 +101,57 @@ string escolheFraseFacil() {
 }
 
 void imprimeCharadaFacil() {
-	cout << escolheFraseFacil();
+	cout << crip(escolheFraseFacil(), limiteIndice());
+	//cout << escolheFraseFacil();
 }
 
 void regras() {
 	cout << "Escrever regras";
+}
+
+// Refatorar o codigo crip e descrip
+string crip(string frase, int chave) {
+	
+	string rest = "";
+	for (int i = 0; i < frase.size(); i++) {
+		for (int j = 0; j < alfa.size(); j++) {
+			if (alfa[j] == frase[i]) {
+				if (j + chave >= 26) {
+					frase[i] = alfa[j + chave - 26];
+				} else {
+					frase[i] = alfa[j + chave];
+				}
+				
+				j = 0;
+				break;
+			}
+		}
+		rest += frase[i];
+	}
+	
+	return rest;
+} 
+
+
+string descrip(string frase, int chave) {
+	string rest = "";
+	for (int i = 0; i < frase.size(); i++) {
+		for (int j = 0; j < alfa.size(); j++) {
+			if (alfa[j] == frase[i]) {
+				if (j - chave < 0) {
+					frase[i] = alfa[j + chave + 26];
+				} else {
+					frase[i] = alfa[j - chave];
+				}
+				
+				j = 0;
+				break;
+			}
+		}
+		rest += frase[i];
+	}
+	
+	return rest;
 }
 
 void jogar() {

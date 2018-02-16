ee61d815020bf960c8725dff043f7b778642d59b
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-02-16 11:29:04 -0300

    adicionado o alfabeto, e regras para acessar elementos de um array pelo indice

--- Paradigma Logico/enigmacesar.pl
+++ Paradigma Logico/enigmacesar.pl
@@ -10,10 +10,17 @@
 
 :- [textos].
 
+/* alfabeto */
+alfabeto(_,X) :- X = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].
+
+/* acessa o elemento associado a um indice */
+getElement(0,[H|_], H).
+getElement(I,[_|T], R) :- X is I-1, getElement(X,T,R).
+
 :- initialization(main).
 
 main :-
 	telaInicial(),nl,
+	desenhaEsfinge(),nl,
 	regras(),nl,
-	desenhaEsfinge,nl,
 	halt(0).

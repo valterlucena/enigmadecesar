a0c068e9a60669789418af0b83caf94e15b7c39e
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-02-20 16:59:54 -0300

    Adicionando fluxo inicial ao jogo.

--- Paradigma Logico/enigmacesar.pl
+++ Paradigma Logico/enigmacesar.pl
@@ -22,8 +22,7 @@ crip(L1, D, L2) :-
 	string_to_list(L2, Y).
 
 /* Aplica o deslocamento D */
-cifra(X, D, Y) :-
-	Y is ((((X - 97) + D) mod 26) + 97). 
+cifra(X, D, Y) :- Y is ((((X - 97) + D) mod 26) + 97). 
 
 /* acessa o elemento associado a um indice */
 getElement(0, [E|_], E).
@@ -31,10 +30,21 @@ getElement(I, [_|T], R) :- X is I-1, getElement(X, T, R).
     
 numero(X) :- random(1, 4, X).
 
+iniciaJogo() :-
+	write('Pressione 1 para as regras ou 2 para jogar.'), nl,
+	read(Opcao), nl,
+	selecao(Opcao). 
+	
+selecao(1) :- regras().
+selecao(2) :- jogar().
+selecao(_) :- write('Voce nao leu direito as instrucoes. Reinicie o jogo!'), nl, halt(0).
+
+jogar() :- write('Buceta').
+
 :- initialization(main).
 
 main :-
 	telaInicial(),nl,
 	desenhaEsfinge(),nl,
-	regras(),nl,
+	iniciaJogo(), nl,
 	halt(0).
